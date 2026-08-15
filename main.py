7f0d2e721bbd0c07874482213df4f76c5b8b6fa5
Sergii Zagoruiko <sergiizag@gmail.com>
2026-08-15 11:37:32 +0300

    1step

---
+++ main.py
@@ -0,0 +1,12 @@
+# Aesthetic Medicine
+
+def main():
+    """1.Вивід тексту меню"""
+
+    print("1. Add client")
+    print("2. Create appointment/order")
+    print("3. Show appointments/orders")
+    print("4. Delete")
+    print("5. Exit")
+
+main()
